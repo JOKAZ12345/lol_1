b5ee7eb7ba0a8d24cae61fc0d77edf4b52a9d032
Jorge Cruz <jokaz12345@gmail.com>
2015-08-12 13:32:21 +0100

    - first version for rito

---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using System.IO;
+
+namespace Riot_API_1
+{
+    public partial class Form1 : Form
+    {
+        private RiotApi riot;
+        private List<string> matchidList; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            riot = new RiotApi();
+            
+            var client = new MongoClient();
+
+            var db = client.GetDatabase("lol");
+
+            var collection = db.GetCollection<BsonDocument>("butchers");
+
+            // Alter this for generic
+            foreach (var json in matchidList.Select(match => riot.GetDataFromUrl(riot.MatchStringBuilder("https://euw.api.pvp.net/api/lol/euw/v2.2", match, true))))
+            {
+                using (var jsonReader = new JsonReader(json))
+                {
+                    var doc = BsonSerializer.Deserialize<BsonDocument>(json);
+                    await AddMongo(doc, collection);
+                }
+            }
+        }
+
+        private static async Task AddMongo(BsonDocument doc, IMongoCollection<BsonDocument> collection)
+        {
+            await collection.InsertOneAsync(doc);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /*var client = new MongoClient();
+
+            var db = client.GetDatabase("lol");
+
+            var collection = db.CreateCollectionAsync("butchers");*/
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var result = openFileDialog1.ShowDialog(); // Show the dialog.
+
+            if (result != DialogResult.OK) return;
+
+            var file = openFileDialog1.FileName;
+
+            try
+            {
+                //string text = File.ReadAllText(file);
+                var text = File.ReadAllLines(file);
+
+                matchidList = new List<string>(text.ToList());
+
+                matchidList.Remove("[");
+                matchidList.Remove("]");
+                matchidList.Remove("\n\r");
+
+                for (var i = 0; i < matchidList.Count; i++)
+                {
+                    if (matchidList[i].Contains(", "))
+                        matchidList[i]= matchidList[i].Replace(", ", "");
+                }
+
+                MessageBox.Show("Succefully loaded " + matchidList.Count + " id's");
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Some error happened!");
+            }
+        }
+    }
+}
---
+++ RiotAPI.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Riot_API_1
+{
+    class RiotApi
+    {
+        private string API_KEY = "";
+
+        // URL ZONES
+        const string EUW = "https://euw.api.pvp.net/api/lol/euw/v2.2";
+        // TODO: ADD MORE
+
+        // WHAT WE NEED ?
+        const string match = "/match";
+        // TODO: ADD MORE
+
+        // timeline by default its true
+        private bool timeLine = true;
+
+
+        // FUNCTIONS
+        public string GetDataFromUrl(string url)
+        {
+            using (var wc = new WebClient())
+            {
+                return wc.DownloadString(url); // TODO: Handle error responses (beh 429)
+            }
+        }
+
+        public string MatchStringBuilder(string zone, string matchId, bool time)
+        {
+            return zone + match + "/" + matchId + "?includeTimeline=" + time.ToString() + "&api_key=" + API_KEY;
+        }
+    }
+}
